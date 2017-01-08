b5e56e367fac41c2acc183cece778563a61f4937
Chris Vig <chris@invictus.so>
2017-01-07 19:12:51 -0600

    Add unit tests to CPP template.

---
+++ cpp/tests/main.cpp
@@ -0,0 +1,11 @@
+/* -- Includes -- */
+
+#include <gtest/gtest.h>
+
+/* -- Procedures -- */
+
+int main(int argc, char** argv)
+{
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
