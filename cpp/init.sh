ef488a92615932821dd3db23c4c9099da9a6f4cb
Chris Vig <chris@invictus.so>
2017-01-08 12:53:52 -0600

    Update build script.

--- cpp/init.sh
+++ cpp/init.sh
@@ -1,7 +1,17 @@
 #!/bin/sh
 set -e
-mkdir -p build/
-cd build/
+
+# Build directory
+BUILD_DIR="./build"
+
+# Recreate build directory
+rm -rf $BUILD_DIR
+mkdir -p $BUILD_DIR
+cd $BUILD_DIR
+
+# Create build system
 cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ..
+
+# Build targets
 make ${TEMPLATE_PROJECT_NAME}
 make ${TEMPLATE_PROJECT_NAME}_tests
