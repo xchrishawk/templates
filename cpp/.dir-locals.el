b19de3cf554530f4ccf17c3a1c10e9f775b813cc
Chris Vig <chris@invictus.so>
2017-01-08 19:57:01 -0600

    Don't require C++ mode for dir-locals.

--- cpp/.dir-locals.el
+++ cpp/.dir-locals.el
@@ -1,2 +1,2 @@
-((c++-mode . ((make-build-dir . "${TEMPLATE_PROJECT_DIR}build")
-              (ff-search-directories . ("${TEMPLATE_PROJECT_DIR}src")))))
+((nil . ((make-build-dir . "${TEMPLATE_PROJECT_DIR}build")
+         (ff-search-directories . ("${TEMPLATE_PROJECT_DIR}src")))))
